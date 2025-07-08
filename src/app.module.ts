6820bf5cd12d722798f7e20c2c5967525a8e0fea
Natalia Taira <nataliagtaira@gmail.com>
2025-07-08 10:44:23 -0300

    Configura banco de dados

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'westwing',
+      database: 'db_crud_farmacia',
+      entities: [],
+      synchronize: true
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
